66a2ec7eabf5023b4f7d53514daba5d23dde9936
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-27 18:54:41 -0300

    Creating the first window!

--- OpenGLStudy/main.cpp
+++ OpenGLStudy/main.cpp
@@ -19,7 +19,50 @@ int main() {
 	//OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Core Profile = No backwards compatibility
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Alow forward compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Main Window", NULL, NULL);
+	if (!mainWindow) {
+		printf("GLFW window creation failed");
+		glfwTerminate();
+		return 1;
+	}
+
+	//Get buffer size
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//Set context to glew use
+	glfwMakeContextCurrent(mainWindow);
+
+	//Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	//Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		printf("GLEW initializaztion failed");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	//Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	//Loop until window closed
+	while (!glfwWindowShouldClose(mainWindow)) {
+		//Get + Handle user input events
+		glfwPollEvents();
+
+		//Clear window
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
 
 	return 0;
 }
